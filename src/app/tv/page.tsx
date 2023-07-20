777854b8182cc927a702a377e5709d1b6953856e
Aidan Timson <contact@timmo.xyz>
2023-07-20 23:11:40 +0100

    Sources

--- src/app/tv/page.tsx
+++ src/app/tv/page.tsx
@@ -14,15 +14,31 @@ const sources: Array<ListItem> = [
     name: "Set-top Box",
     icon: <ServerIcon className="h-6 w-6 text-gray-400" />,
   },
+  {
+    name: "Disney+",
+    icon: <span className="text-lg">D+</span>,
+  },
+  {
+    name: "Netflix",
+    icon: <span className="mr-3 text-lg">N</span>,
+  },
+  {
+    name: "Prime Video",
+    icon: <span className="mr-3 text-lg">P</span>,
+  },
+  {
+    name: "YouTube",
+    icon: <span className="mr-3 text-lg">Y</span>,
+  },
 ];
 
 export default async function Home() {
   return (
     <>
-      <h2 className="text-2xl font-bold">Sources</h2>
-      <List items={sources} />
       <h2 className="mt-4 text-2xl font-bold">Remote</h2>
       <Remote />
+      <h2 className="text-2xl font-bold">Sources</h2>
+      <List items={sources} />
     </>
   );
 }
