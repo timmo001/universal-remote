32ad0a12c7ebaa15442bf1b0e34a2c4cceef1894
Aidan Timson <contact@timmo.xyz>
2023-07-20 23:08:22 +0100

    Remote

--- src/components/remote.tsx
+++ src/components/remote.tsx
@@ -1,12 +1,23 @@
 "use client";
 import { MouseEvent, MouseEventHandler } from "react";
 import {
-  PowerIcon,
-  PlusIcon,
-  SpeakerXMarkIcon,
-  ChevronUpIcon,
-  MinusIcon,
+  ArrowUturnLeftIcon,
+  BackwardIcon,
   ChevronDownIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  ChevronUpIcon,
+  Cog6ToothIcon,
+  ForwardIcon,
+  HomeIcon,
+  MinusIcon,
+  PauseIcon,
+  PlayIcon,
+  PlusIcon,
+  PowerIcon,
+  SpeakerXMarkIcon,
+  StopIcon,
+  TvIcon,
 } from "@heroicons/react/24/outline";
 
 function Button({
@@ -32,14 +43,14 @@ export default function Remote({}: {}) {
 
   return (
     <>
-      <section className="grid min-w-full grid-cols-1 gap-4">
+      <section className="grid min-w-full grid-cols-1 gap-x-2 gap-y-4">
         <Button
           name="power"
           icon={<PowerIcon className="h-6 w-6 text-red-600" />}
           onClick={handleButtonClick}
         />
       </section>
-      <section className="grid min-w-full grid-cols-3 gap-4">
+      <section className="grid min-w-full grid-cols-3 gap-x-2 gap-y-4">
         {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((number) => (
           <Button
             key={number}
@@ -56,7 +67,7 @@ export default function Remote({}: {}) {
         />
         <div />
       </section>
-      <section className="grid min-w-full grid-cols-3 gap-4">
+      <section className="grid min-w-full grid-cols-3 gap-x-2 gap-y-4">
         <Button
           name="volumeUp"
           icon={<PlusIcon className="h-6 w-6" />}
@@ -81,6 +92,109 @@ export default function Remote({}: {}) {
         <Button
           name="channelDown"
           icon={<ChevronDownIcon className="h-6 w-6" />}
+          onClick={handleButtonClick}
+        />
+      </section>
+      <section className="grid min-w-full grid-cols-3 gap-x-2 gap-y-4">
+        <Button
+          name="home"
+          icon={<HomeIcon className="h-6 w-6" />}
+          onClick={handleButtonClick}
+        />
+        <Button
+          name="up"
+          icon={<ChevronUpIcon className="h-6 w-6" />}
+          onClick={handleButtonClick}
+        />
+        <Button
+          name="settings"
+          icon={<Cog6ToothIcon className="h-6 w-6" />}
+          onClick={handleButtonClick}
+        />
+        <Button
+          name="left"
+          icon={<ChevronLeftIcon className="h-6 w-6" />}
+          onClick={handleButtonClick}
+        />
+        <Button
+          name="ok"
+          icon={<span className="text-2xl">OK</span>}
+          onClick={handleButtonClick}
+        />
+        <Button
+          name="right"
+          icon={<ChevronRightIcon className="h-6 w-6" />}
+          onClick={handleButtonClick}
+        />
+        <Button
+          name="back"
+          icon={<ArrowUturnLeftIcon className="h-6 w-6" />}
+          onClick={handleButtonClick}
+        />
+        <Button
+          name="down"
+          icon={<ChevronDownIcon className="h-6 w-6" />}
+          onClick={handleButtonClick}
+        />
+        <Button
+          name="guide"
+          icon={<TvIcon className="h-6 w-6" />}
+          onClick={handleButtonClick}
+        />
+      </section>
+      <section className="grid min-w-full grid-cols-4 justify-between gap-x-2 gap-y-4">
+        <Button
+          name="rewind"
+          icon={<BackwardIcon className="h-6 w-6" />}
+          onClick={handleButtonClick}
+        />
+        <Button
+          name="play"
+          icon={<PlayIcon className="h-6 w-6" />}
+          onClick={handleButtonClick}
+        />
+        <Button
+          name="pause"
+          icon={<PauseIcon className="h-6 w-6" />}
+          onClick={handleButtonClick}
+        />
+        <Button
+          name="fastForward"
+          icon={<ForwardIcon className="h-6 w-6" />}
+          onClick={handleButtonClick}
+        />
+      </section>
+      <section className="grid min-w-full grid-cols-2 justify-between gap-x-2 gap-y-4">
+        <Button
+          name="stop"
+          icon={<StopIcon className="h-6 w-6" />}
+          onClick={handleButtonClick}
+        />
+        <Button
+          name="record"
+          icon={<span className="h-5 w-5 rounded-full bg-red-600" />}
+          onClick={handleButtonClick}
+        />
+      </section>
+      <section className="grid min-w-full grid-cols-4 gap-x-2 gap-y-4">
+        <Button
+          name="red"
+          icon={<span className="h-4 w-8 rounded-full bg-red-600" />}
+          onClick={handleButtonClick}
+        />
+        <Button
+          name="green"
+          icon={<span className="h-4 w-8 rounded-full bg-green-600" />}
+          onClick={handleButtonClick}
+        />
+        <Button
+          name="yellow"
+          icon={<span className="h-4 w-8 rounded-full bg-yellow-600" />}
+          onClick={handleButtonClick}
+        />
+        <Button
+          name="blue"
+          icon={<span className="h-4 w-8 rounded-full bg-blue-600" />}
           onClick={handleButtonClick}
         />
       </section>
