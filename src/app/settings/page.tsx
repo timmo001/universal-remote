c6d1a616daba55f8e88b06c93e0ec5957615774c
Aidan Timson <contact@timmo.xyz>
2023-07-21 16:37:37 +0100

    Settings

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,28 +1,36 @@
 "use client";
 import { ChangeEvent, ChangeEventHandler } from "react";
-import { HomeIcon } from "@heroicons/react/24/outline";
+import {
+  LightBulbIcon,
+  MusicalNoteIcon,
+  TvIcon,
+} from "@heroicons/react/24/outline";
 
 import { useSettings } from "@/providers/settings";
 
 function TextInput({
+  name,
+  label,
+  icon,
   value,
   handleChange,
 }: {
+  name: string;
+  label?: string;
+  icon?: JSX.Element;
   value: string;
   handleChange: ChangeEventHandler<HTMLInputElement>;
 }) {
   return (
     <label className="block w-full">
-      <span className="flex flex-row items-center gap-3">
-        <HomeIcon className="h-6 w-6 text-gray-400" />
-        <span className="block text-sm font-medium text-gray-200">
-          TV Entities
-        </span>
+      <span className="flex flex-row items-center gap-2">
+        {icon}
+        <span className="block text-sm font-medium text-gray-200">{label}</span>
       </span>
       <input
         className="mt-1 block w-full rounded-md border-gray-900 bg-gray-900 px-2 py-1 text-gray-200 shadow-sm focus:border-slate-900 focus:ring focus:ring-slate-900 focus:ring-opacity-50"
         type="text"
-        name="tv.entities"
+        name={name}
         onChange={handleChange}
         value={value}
       />
@@ -47,9 +55,26 @@ export default function Settings() {
 
   return (
     <>
-      <form className="w-full">
+      <form className="flex w-full flex-col gap-4">
         <TextInput
+          name="tv.entities"
+          label="TV Entities"
+          icon={<TvIcon className="h-6 w-6 text-gray-200" />}
           value={settings?.tv?.entities?.join(",") || ""}
+          handleChange={handleChange}
+        />
+        <TextInput
+          name="music.entities"
+          label="Music Entities"
+          icon={<MusicalNoteIcon className="h-6 w-6 text-gray-200" />}
+          value={settings?.music?.entities?.join(",") || ""}
+          handleChange={handleChange}
+        />
+        <TextInput
+          name="lights.entities"
+          label="Light Entities"
+          icon={<LightBulbIcon className="h-6 w-6 text-gray-200" />}
+          value={settings?.lights?.entities?.join(",") || ""}
           handleChange={handleChange}
         />
       </form>
--- src/app/setup/page.tsx
+++ src/app/setup/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { ChangeEvent, useEffect, useState } from "react";
-import { HomeIcon } from "@heroicons/react/24/outline";
 
 import { useSettings } from "@/providers/settings";
 import { useRouter } from "next/navigation";
@@ -23,7 +22,6 @@ export default function Setup() {
     console.log("Current settings:", settings);
     settings.homeAssistant.url = homeAssistantUrl;
     updateSettings(settings);
-    // console.log("Saved settings:", settings);
     router.push("/");
     router.refresh();
   }
@@ -31,26 +29,18 @@ export default function Setup() {
   return (
     <>
       <h2 className="mb-2 text-2xl font-bold">
-        Please enter Home Assistant url
+        Please enter Home Assistant URL
       </h2>
-      <form>
-        <label className="block">
-          <span className="flex flex-row items-center gap-3">
-            <HomeIcon className="h-6 w-6 text-gray-400" />
-            <span className="block text-sm font-medium text-gray-200">
-              Home Assistant URL
-            </span>
-          </span>
-          <input
-            className="mt-1 block w-full rounded-md border-gray-900 bg-gray-900 px-2 py-1 text-gray-200 shadow-sm focus:border-slate-900 focus:ring focus:ring-slate-900 focus:ring-opacity-50"
-            type="text"
-            autoFocus
-            name="homeAssistant.url"
-            aria-label="Home Assistant URL"
-            onChange={handleChange}
-            value={homeAssistantUrl}
-          />
-        </label>
+      <form className="flex w-full flex-col gap-4">
+        <input
+          className="mt-1 block w-full rounded-md border-gray-900 bg-gray-900 px-2 py-1 text-gray-200 shadow-sm focus:border-slate-900 focus:ring focus:ring-slate-900 focus:ring-opacity-50"
+          type="text"
+          autoFocus
+          name="homeAssistant.url"
+          aria-label="Home Assistant URL"
+          onChange={handleChange}
+          value={homeAssistantUrl}
+        />
       </form>
       <button
         className="mt-2 rounded-md border border-transparent bg-slate-900 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-slate-800 focus:outline-none focus:ring-2 focus:ring-slate-900 focus:ring-offset-2"
