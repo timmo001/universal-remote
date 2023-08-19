736833d17e677db35ccbc492f0b3204f25bb9b1a
Aidan Timson <contact@timmo.xyz>
2023-08-19 22:57:06 +0100

    Revert "Cleanup"
    This reverts commit c441986a35841bd4cc5d40e5304eecad7b2c94fa.

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -6,20 +6,22 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
 
   return (
-    <AnimatePresence initial={false} mode="wait">
-      <motion.div
-        key={pathname}
-        initial={{ x: 300, opacity: 0 }}
-        animate={{ x: 0, opacity: 1 }}
-        exit={{ x: -100, opacity: 0 }}
-        transition={{
-          type: "spring",
-          stiffness: 260,
-          damping: 20,
-        }}
-      >
-        {children}
-      </motion.div>
-    </AnimatePresence>
+    <div className="effect-2">
+      <AnimatePresence initial={false} mode="wait">
+        <motion.div
+          key={pathname}
+          initial={{ x: 300, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          exit={{ x: -100, opacity: 0 }}
+          transition={{
+            type: "spring",
+            stiffness: 260,
+            damping: 20,
+          }}
+        >
+          {children}
+        </motion.div>
+      </AnimatePresence>
+    </div>
   );
 }
