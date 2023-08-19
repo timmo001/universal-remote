c441986a35841bd4cc5d40e5304eecad7b2c94fa
Aidan Timson <contact@timmo.xyz>
2023-08-19 22:52:39 +0100

    Cleanup

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -6,22 +6,20 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
 
   return (
-    <div className="effect-2">
-      <AnimatePresence initial={false} mode="wait">
-        <motion.div
-          key={pathname}
-          initial={{ x: 300, opacity: 0 }}
-          animate={{ x: 0, opacity: 1 }}
-          exit={{ x: -100, opacity: 0 }}
-          transition={{
-            type: "spring",
-            stiffness: 260,
-            damping: 20,
-          }}
-        >
-          {children}
-        </motion.div>
-      </AnimatePresence>
-    </div>
+    <AnimatePresence initial={false} mode="wait">
+      <motion.div
+        key={pathname}
+        initial={{ x: 300, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        exit={{ x: -100, opacity: 0 }}
+        transition={{
+          type: "spring",
+          stiffness: 260,
+          damping: 20,
+        }}
+      >
+        {children}
+      </motion.div>
+    </AnimatePresence>
   );
 }
